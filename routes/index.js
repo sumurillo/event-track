162ece28b894a2ab9185eb7867d20efcec65e9b6
sumurillo <sumire.ohana@gmail.com>
2023-02-24 16:02:26 -0600

    got login to show and work

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+//Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    prompt: 'select_account'
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/movies', // change path later
+    failureRedirect: '/movies'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout(function() {
+    //change path later
+    res.redirect('/movies')
+  });
 });
 
 module.exports = router;
