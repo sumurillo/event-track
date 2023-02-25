b72a09177ad66c492455dd0530469146fd03db71
sumurillo <sumire.ohana@gmail.com>
2023-02-25 13:41:18 -0600

    eventSchema added

--- models/event.js
+++ models/event.js
@@ -5,5 +5,29 @@ const eventSchema = new Schema({
     name: {
         type: String,
         required: true
-    }
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    guest: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    userName: String,
+    userAvatar: String
 });
